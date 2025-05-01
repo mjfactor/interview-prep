1736c278c3aa901475305bbdc98cbcc0bd0efef9
mjfactor <emjayfactor@gmail.com>
2025-05-01 12:36:59 +0800

    add dark model

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,8 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Toaster />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -3,6 +3,9 @@
 import {
   ChevronsUpDown,
   LogOut,
+  Sun,
+  Moon,
+  Monitor,
 } from "lucide-react"
 
 import {
@@ -13,13 +16,14 @@ import {
 import {
   DropdownMenu,
   DropdownMenuContent,
-
-  DropdownMenuGroup,
-
+  DropdownMenuSub,
+  DropdownMenuPortal,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
 } from "@/components/ui/dropdown-menu"
 import {
   SidebarMenu,
@@ -28,6 +32,7 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar"
 import { useSignOut } from "@/hooks/firebase-hooks"
+import { useTheme } from "next-themes"
 export function NavUser({
   user,
 }: {
@@ -37,6 +42,7 @@ export function NavUser({
     avatar: string
   }
 }) {
+  const { setTheme } = useTheme()
   const { isMobile } = useSidebar()
   const signOut = useSignOut();
   return (
@@ -78,8 +84,33 @@ export function NavUser({
               </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
-           
-            <DropdownMenuGroup/>
+
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <Sun className="mr-2 h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute mr-2 h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span>Theme</span>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem onClick={() => setTheme("light")}>
+                    <Sun className="mr-2 h-4 w-4" />
+                    <span>Light</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    <Moon className="mr-2 h-4 w-4" />
+                    <span>Dark</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setTheme("system")}>
+                    <Monitor className="mr-2 h-4 w-4" />
+                    <span>System</span>
+                  </DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+
+
+            <DropdownMenuSeparator />
             <DropdownMenuItem onSelect={signOut} >
               <LogOut />
               Log out
